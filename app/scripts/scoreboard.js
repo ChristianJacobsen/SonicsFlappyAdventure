ef86dbcea966173b064a12631f5d130125f1bf5a
Hilmar Tryggvason <hilmart@gmail.com>
2017-03-27 13:21:38 +0000

    Fixed scoreboard speedhack

--- app/scripts/scoreboard.js
+++ app/scripts/scoreboard.js
@@ -1,13 +1,27 @@
 window.Scoreboard = (function () {
     "use strict";
 
-    const SPACEBAR_WAIT = 500; // Milliseconds of how long the spacebar will not restart the game
+    const WAIT = 500; // Milliseconds of how long the spacebar will not restart the game
 
     var Scoreboard = function (el, game) {
         this.el = el;
         this.highSpan = this.el.find(".highscore > span"); // Span for highscore value
         this.currentSpan = this.el.find(".currentscore > span"); // Span for current score balue
         this.game = game;
+        this.visible = false;
+
+        let scoreboard = this;
+        this.el.find("button").on("click", function () {
+            if (scoreboard.visible) {
+                scoreboard.restart();
+            }
+        });
+
+        document.body.onkeyup = function (e) {
+            if (scoreboard.visible && e.keyCode === 32) {
+                scoreboard.restart();
+            }
+        };
     };
 
     /**
@@ -17,29 +31,21 @@ window.Scoreboard = (function () {
         this.highSpan.text(this.game.highscore); // Set highscore
         this.currentSpan.text(this.game.score); // Set current score
 
-        // Show scoreboard and add click event
-        let scoreboard = this;
-        this.el.addClass("is-visible")
-            .find("button")
-            .one("click", function () {
-                scoreboard.restart();
-            });
+        // Show scoreboard
+        this.el.addClass("is-visible");
 
-        // Wait a bit until enabling spacebar for restarting
+        // Wait a bit until enabling restarting
+        let scoreboard = this;
         setTimeout(function () {
-            document.body.onkeyup = function (e) {
-                if (e.keyCode === 32) {
-                    scoreboard.restart();
-                }
-            };
-        }, SPACEBAR_WAIT);
+            scoreboard.visible = true;
+        }, WAIT);
     };
 
     /**
      * Hides the scoreboard and restarts the game
      */
     Scoreboard.prototype.restart = function () {
-        document.body.onkeyup = null;
+        this.visible = false;
         this.el.removeClass("is-visible");
         this.game.start();
     };
